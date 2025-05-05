3fd98c5beddfdcfd172ad6be8538d74a280eee26
Jama <jama6833@gmail.com>
2025-05-05 15:23:03 +0200

    header komponent

--- src/Komponent/Header.jsx
+++ src/Komponent/Header.jsx
@@ -1,1 +1,11 @@
 import { useLocation, useNagigate } from "react-router-dom";
+const Header =() => {
+    const location = useLocation();
+    const navigate = useNagigate();
+
+    const Startover = () => {
+        navigate("/");
+    };
+
+    const showStartOver = location.pathname === "/Showgame"
+}
