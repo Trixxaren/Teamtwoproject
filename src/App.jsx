cfaa490ae9e764975142f14494be8618dfec31a6
Jama <jama6833@gmail.com>
2025-05-05 21:32:21 +0200

    Fixade layout i Header och App-komponenter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Komponent/Header";
-const App = () => {
-  return <p>Hello World!</p>;
 
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<div>Startsida</div>} />
+        <Route path="/Showgame" element={<div>Show Game sida</div>} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
+
--- src/Komponent/Header.jsx
+++ src/Komponent/Header.jsx
@@ -1,11 +1,23 @@
-import { useLocation, useNagigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 const Header =() => {
     const location = useLocation();
-    const navigate = useNagigate();
+    const navigate = useNavigate();
 
     const Startover = () => {
         navigate("/");
     };
-
-    const showStartOver = location.pathname === "/Showgame"
+    
+    const showStartOver = location.pathname === "/Showgame";
+    
+    return (
+        <header>
+            <h1>Free to Game</h1>
+            {showStartOver && (
+                <button onClick={Startover}>
+                    Start Over
+                </button>
+            )}
+        </header>
+    );
 }
+export default Header;
