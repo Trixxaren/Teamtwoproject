fe49b01338415816dbf51c67526ab6ef1ad60634
Jama <jama6833@gmail.com>
2025-05-05 15:04:46 +0200

    Lagt till Header-komponenten

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import {BrowserRouter as Router, Routes, Route } from "react-router-dem";
+import Header from "./Komponent/Header";
 const App = () => {
   return <p>Hello World!</p>;
+  
 };
 
 export default App;
